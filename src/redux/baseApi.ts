dc1bc35fa125dc2bccc1c44bddc669af0612362d
sakincse21 <saleheen.sakin@gmail.com>
2025-08-22 21:57:49 +0600

    baseurl fix in rtk query

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: envVars.backendBaseUrl,
+    baseUrl: `${envVars.backendBaseUrl}/api/v1`,
     credentials: "include",
   }),
   tagTypes: ["USER", "TRANSACTIONS", "DIVISION"],
