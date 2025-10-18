133c4cd41ffb68d269bbd96017aa48efe1e479b4
sakincse21 <saleheen.sakin@gmail.com>
2025-10-19 00:53:55 +0600

    added url redirect after login

--- src/lib/withAuth.tsx
+++ src/lib/withAuth.tsx
@@ -1,16 +1,20 @@
 import { useUserInfoQuery } from "@/redux/features/User/user.api";
 import type { TRole } from "@/types/user";
 import type { ComponentType } from "react";
-import { Navigate } from "react-router";
+import { Navigate, useLocation } from "react-router";
 
 export const WithAuth = (Component: ComponentType, requiredRoles?: TRole[]) => {
   return function AuthWrapper() {
     // console.log(requiredRoles);
 
     const { data, isLoading } = useUserInfoQuery(undefined);
+    const location = useLocation();
 
     if (!isLoading && !data?.data?.email) {
-      return <Navigate to={"/login"} />;
+      // return <Navigate to={"/login"} />;
+      return (
+        <Navigate to="/login" state={{ from: location.pathname }} replace />
+      );
     }
     if (!isLoading && !requiredRoles?.includes(data?.data?.role)) {
       return <Navigate to={"/unauthorized"} />;
